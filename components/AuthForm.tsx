6a9de430df8b07999d68da7f335627ab7c237878
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2023-11-29 16:04:18 +0100

    auth test

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,10 +7,9 @@ import {Database} from "@/types/supabaseDatabaseTypes";
 export default function AuthForm() {
     const supabase = createClientComponentClient<Database>()
     const getURL = () => {
-        let url =
-            process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-            process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-            'http://localhost:3000/'
+        let url = process?.env?.NEXT_PUBLIC_VERCEL_ENV === 'production' ?
+            "https://caloriecompas.ch" :
+            process?.env?.NEXT_PUBLIC_VERCEL_URL!// Automatically set by Vercel.
         // Make sure to include `https://` when not localhost.
         url = url.includes('http') ? url : `https://${url}`
         // Make sure to include a trailing `/`.
