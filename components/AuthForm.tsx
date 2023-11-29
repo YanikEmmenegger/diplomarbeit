0645ab85f6d0330c82c75bf64291ea3e7cd31e53
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2023-11-29 16:16:00 +0100

    auth test with domain

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,7 +18,7 @@ export default function AuthForm() {
     }
     return (
         <div id={"cc-auth-form"} className=" pt-12 mx-auto lg:w-1/2 w-[90%]">
-            <h1 className={"text-center text-3xl"}>{process?.env?.NEXT_PUBLIC_VERCEL_ENV} Login | SignUp {getURL()}</h1>
+            <h1 className={"text-center text-3xl"}>Login | SignUp</h1>
             <Auth socialLayout={"vertical"} magicLink providers={["linkedin", "github", "apple", "google"]}
                   redirectTo={getURL()}
                   onlyThirdPartyProviders
